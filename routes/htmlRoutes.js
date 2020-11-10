8c91a0cbdaf90c8387a2732aad882a3587311154
Chris Kalama <chriskalama96@gmail.com>
2020-11-09 23:19:55 -0600

    Added default * get call to complete htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,9 +2,14 @@ var path = require ("path");
 
 module.exports = function(app) {
 
+        //Remember, the callback function has to be req,res and not vice versa. Otherwise it wont' work. 
     app.get("/notes", (req,res) => {
         res.sendFile(path.join(__dirname, "../public/notes.html"))
     });
+
+    app.get("*", (req,res)=> {
+        res.sendFile(path.join(__dirname, "../public/index.html"))
+    })
 
 
 
