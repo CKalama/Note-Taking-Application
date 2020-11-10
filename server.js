4da951a5123a92ad94b2182eec82fb05a373871d
Chris Kalama <chriskalama96@gmail.com>
2020-11-09 22:26:44 -0600

    set server.js with app.listen function to fire up server

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
 const express = require("express");
+
+
+//Attaching app to express for routing. 
+var app = express();
+
+//Initializing port
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Setting up Routes, these provide a user with where the browser will "route" to when clicking an element on the browser
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("Server fired up at localhost:" + PORT);
+})
