c6d1f371867eea46f61a12883bfdb0732062cbf6
Chris Kalama <chriskalama96@gmail.com>
2020-11-10 21:42:18 -0600

    with Poornima, was able to write first app.post route in apiRoutes. Using JSON and fs to write a file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,6 @@
 
 var dbData = require("../db/db.json")
+var fs = require("fs");
 
 
 module.exports = function(app) {
@@ -7,7 +8,30 @@ module.exports = function(app) {
         res.json(dbData);
     })
 
-    app.post("/api/notes", (req,res) => {
+   app.post("/api/notes", (req,res) => {
+        var newNote = req.body;
+        console.log(newNote);
+
+        fs.readFile("./db/db.json", "UTF-8", function(err, data) {
+            if (err) {
+                console.log(err)
+            } 
+                console.log(data)
+            //Converting into an array
+            data = JSON.parse(data);
+            //Pushing req.body into new data parameter
+            data.push(newNote);
+            console.log(data);
+         fs.writeFile("./db/db.json", JSON.stringify(data), function(err) {
+             if(err) {
+                 console.log(err)
+             } else {
+                 console.log("Success!!!!");
+             }
+         })        
+        }) 
+        
+        
         
     })
 
--- server.js
+++ server.js
@@ -11,8 +11,9 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
-//Exporting Routes, these provide a user with where the browser will "route" to when clicking an element on the browser
+//Exporting Routes, these provide a user with where the browser will "route" to when clicking an element on the browser. These won't work unless you have your module.export all good to go
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
